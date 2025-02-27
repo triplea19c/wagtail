1e6f2b5ac4bea81490f8f80b548f3389910afc18
Andy Agbetsiafa <triplea19c@gmail.com>
2025-02-27 15:58:02 +0000

    Added a blog to the blog app and added the BlogPage model

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,21 @@
 from django.db import models
-
-# Create your models here.
 from wagtail.models import Page
 from wagtail.fields import RichTextField
+from wagtail.search import index
 
 class BlogIndexPage(Page):
     intro = RichTextField(blank=True)
 
     content_panels = Page.content_panels + ["intro"]
+
+class BlogPage(Page):
+    date = models.DateField("Post date")
+    intro = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    ]
+
+    content_panels = Page.content_panels + ["date", "intro", "body"]
